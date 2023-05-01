907fe5004bc0cc7f92af7fd0d7499e93d7f749be
Steven <servingbaby@users.noreply.github.com>
2023-05-01 17:14:54 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,3 @@
-# Python to run Suno-ai's Text-to-Speech API - BARK.
-
-# Thank you for the token counter idea and intial code to play with @
-# https://github.com/suno-ai/bark/pull/84
-
 import datetime
 from bark import SAMPLE_RATE, generate_audio, preload_models
 from scipy.io.wavfile import write as write_wav
@@ -25,7 +20,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers')
         self.end_headers()
 
         # Get the user input from the request body
@@ -60,7 +55,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         combined_audio = np.concatenate(audio_arrays)
 
         # Write the combined audio to a file
-        timestamp_str = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        # timestamp_str = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
         # filename = f"Bark_audio_{timestamp_str}.wav"
         filename = './audio/bark_audio.wav'
         write_wav(filename, SAMPLE_RATE, combined_audio)
@@ -70,8 +65,8 @@ class RequestHandler(BaseHTTPRequestHandler):
 
         # Send a response back to the client
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        # self.send_header('Content-type', 'audio/wav')
+        # self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'audio/wav')
         self.end_headers()
         # self.wfile.write(b'String sent to Bark\n')
         with open(filename, 'rb') as f:
@@ -80,8 +75,9 @@ class RequestHandler(BaseHTTPRequestHandler):
     def do_OPTIONS(self):
         self.send_response(200, "ok")
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
         self.send_header('Access-Control-Allow-Headers', 'X-Requested-With')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
         self.end_headers()
 
 def do_GET(self):
@@ -93,16 +89,47 @@ def do_GET(self):
         self.end_headers()
         with open('index.html', 'rb') as f:
             self.wfile.write(f.read())
-    elif self.path == '/audio':
+    elif self.path == '/audio/bark_audio.wav':
         # Return the audio file
         self.send_response(200)
         self.send_header('Content-type', 'audio/wav')
         self.end_headers()
         with open('./audio/bark_audio.wav', 'rb') as f:
             self.wfile.write(f.read())
+        # Return the audio blob
+        #audio_blob = get_audio_blob()  # function to get the audio blob
+        #self.send_response(206)
+        #self.send_header('Content-type', 'audio/wav')
+        #self.send_header('Content-Range', 'bytes 0-{0}/{0}'.format(len(audio_blob)-1))
+        #self.send_header('Content-Length', str(len(audio_blob)))
+        #self.end_headers()
+        #self.wfile.write(audio_blob)
     else:
         # Return a 404 error
         self.send_error(404, 'File Not Found')
+
+
+def do_DELETE(self):
+    # Set CORS headers
+    self.send_response(200)
+    self.send_header('Access-Control-Allow-Origin', '*')
+    # self.send_header('Access-Control-Allow-Methods', 'POST, DELETE, OPTIONS')
+    self.send_header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers')
+    self.end_headers()
+
+    # Get the path of the requested file
+    file_path = '.' + self.path
+
+    # Check if the file exists
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'File deleted')
+    else:
+        self.send_response(404)
+        self.end_headers()
+        self.wfile.write(b'File not found')
 
 
 # Start the HTTP server
